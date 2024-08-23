ba3af52ac3d8e22c52511150ebce724265b39b10
laure1102 <40697237+laure1102@users.noreply.github.com>
2024-08-23 10:42:31 +0800

    Update db_config.js

--- server/config/db_config.js
+++ server/config/db_config.js
@@ -5,8 +5,8 @@ module.exports = {
   development: {
     database: "vyoke",
     username: "postgres",
-    password: "laure",
-    host: "106.54.232.179",
+    password: "xxxx",
+    host: "xxxxxxx",
     port: 5432,
     dialect: "postgres",
     logging: true,
